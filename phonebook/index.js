0e1bc814cd53f09b9e79956e431e2326acb4dcb2
Detroix23 <detroix23@gmail.com>
2025-11-09 13:21:01 +0100

    ex3.1.

---
+++ phonebook/index.js
@@ -0,0 +1,50 @@
+// index.js
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+// Hardcoded data.
+const persons = [
+    { 
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    { 
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    { 
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+];
+
+app.get('/', (_, response) => {
+  console.log('index.app.get - / - Greetings.')
+  response.send(`
+    <h1>Phonebook backend.</h1>
+    <a href="./api/persons">See <code>/api</a>
+  `);
+});
+
+app.get('/api/persons', (request, response) => {
+  console.log('index.app.get - /api/persons/ - Responding with persons:', persons);
+
+  response.json(persons);
+});
+
+
+const PORT = 3001;
+
+app.listen(PORT);
+console.log(`index - 'Phonebook' server running on port ${PORT}.`);
